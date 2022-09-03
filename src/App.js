fc41627aec9774cbe8d1b69a68c856375d492396
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2022-09-03 19:58:21 +0600

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Марья",
+  lastName: "Моревна",
+};
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Здравствуй, {formatName(user)}!</h1>;
+  }
+  return <h1>Здравствуй, незнакомец.</h1>;
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <h1>Здравствуй, {formatName(user)}!</h1>
+      </h1>
     </div>
   );
 }
